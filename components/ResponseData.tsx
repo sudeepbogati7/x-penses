db5d38785b8b7a415e4d4b890af4880bf1071ea8
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-06-27 11:35:26 +0545

    landing page

---
+++ components/ResponseData.tsx
@@ -0,0 +1,41 @@
+import React, { createContext, useState, useContext } from 'react';
+interface User {
+    userId: number;
+    fullName: string;
+    email: string;
+    updatedAt: string;
+    createdAt: string;
+    confirmPassword: null;
+}
+
+interface ResponseData {
+    message: string;
+    user: User;
+    token: string;
+}
+
+// Define an initial context value
+const initialContextValue: {
+    responseData: ResponseData | null;
+    setResponseData: React.Dispatch<React.SetStateAction<ResponseData | null>>;
+} = {
+    responseData: null,
+    setResponseData: () => { } // Dummy function that does nothing
+};
+
+// Create the context
+const ResponseDataContext = createContext(initialContextValue);
+
+// Custom hook to access the context value
+export const useResponseData = () => useContext(ResponseDataContext);
+
+// Context provider component
+export const ResponseDataProvider: any = ({ children }: any) => {
+    const [responseData, setResponseData] = useState<ResponseData | null>(null);
+
+    return (
+        <ResponseDataContext.Provider value={{ responseData, setResponseData }}>
+            {children}
+        </ResponseDataContext.Provider>
+    );
+};
