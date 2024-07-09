e3297b96807e94d9c64d147817760185909f1076
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-07-09 19:42:53 +0545

    minor changes

--- app/register/layout.tsx
+++ app/register/layout.tsx
@@ -1,5 +1,4 @@
 'use client';
-// import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from '../theme-provider';
 import { ThemeSwitcher } from '@/components/ThemeSwitcher';
